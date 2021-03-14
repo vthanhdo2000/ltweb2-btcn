5bcb80958e1faef117c269e9f8403cf2c5a3717a
vthanhdo2000 <vthanhdo2000@gmail.com>
2021-03-14 12:06:43 +0700

    hello

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')));
+//app.use(express.static(path.join(__dirname,'public')));
+app.get('/', (req, res) => {
+    res.send('<h1>hello world</h1>');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
